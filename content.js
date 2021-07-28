f63aceff5e72c9952079fb86abab5ccd9eed4fdb
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-07-28 19:27:04 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+alert("Welcome to Pigeon!");
+
+/* what to do:
+    - we'll want to start off by simply having an image become opique when hovering over a link.
+    - next, we'll want to have it so it illustrates one link and where it leads to.
+*/
