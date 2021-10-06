acfc2546a1146606c5b838d2bc02f6190def9773
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-10-06 19:26:03 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
-alert("Welcome to Pigeon!");
+alert("Welcome to Mole!");
 
 /* what to do:
     - we'll want to start off by simply having an image become opique when hovering over a link.
     - next, we'll want to have it so it illustrates one link and where it leads to.
+    - Make javascript function that grabs the link properties.
+    - https://stackoverflow.com/questions/1226714/how-to-get-the-browser-to-navigate-to-url-in-javascript
+    - https://towardsdatascience.com/quickly-extract-all-links-from-a-web-page-using-javascript-and-the-browser-console-49bb6f48127b
 */
